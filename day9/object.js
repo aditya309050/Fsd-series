88eb0d9b4aba0f860e10c6b27b619dde67fd65f2
adityaraj309050 <adityaraj309050@gmail.com>
2026-09-03 09:51:31 +0530

    03

---
+++ day9/object.js
@@ -0,0 +1,15 @@
+let mobile = {
+  brand: "Samsung",
+  model: "Galaxy S21",
+  color: "Black",
+  price: 799.99
+};
+console.log(mobile.brand); // Output: Samsung
+console.log(mobile.model); // Output: Galaxy S21
+console.log(mobile.color); // Output: Black
+console.log(mobile.price); // Output: 799.99
+let intro ={
+    "first name": "aditya",
+    "last name": "kumar",
+}
+console.log(intro["first name"]); // Output: aditya
